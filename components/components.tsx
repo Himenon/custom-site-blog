3ddb7fd04559805a0ea12953d650ee13342eb820
Himenon <6715229+Himenon@users.noreply.github.com>
2019-01-05 02:59:39 +0900

    Syntax Highlightが付くように

--- components/components.tsx
+++ components/components.tsx
@@ -2,15 +2,46 @@
 /// <reference path="../node_modules/custom-site/typings/@mdx-js/index.d.ts"/>
 
 import { CustomComponents } from "@mdx-js/tag";
+import * as Prism from "prismjs";
 import * as React from "react";
+
+/**
+ * ハイライトするコードをrequireしておく
+ */
+require("prismjs/components/prism-typescript.min.js")
+
+const SUPPORT_LANGUAGES = Object.keys(Prism.languages);
+
+const getLanguageDefinition = (lang: string): Prism.LanguageDefinition | null => {
+  if (SUPPORT_LANGUAGES.includes(lang)) {
+    return Prism.languages[lang];
+  }
+  return null;
+}
 
 export const customComponents = (): CustomComponents => {
   return {
+    pre: props => <>{ props.children }</>,
     code: props => {
+      const lang = props.className ? props.className.slice("language-".length) : "";
+      const code = typeof props.children === "string" ? props.children : "";
+      const grammar = getLanguageDefinition(lang);
+      let highlightHtml: string | undefined;
+      if (grammar) {
+        highlightHtml = Prism.highlight(code, grammar);
+      }
+      const newProps: JSX.IntrinsicElements["code"] = {
+        ...props,
+        children: undefined,
+        dangerouslySetInnerHTML: {
+          __html: highlightHtml
+        }
+      };
       return (
-        <code {...props} />
+      <pre className={props.className}>
+        <code {...newProps} />
+      </pre>
       );
     },
   };
 };
-
